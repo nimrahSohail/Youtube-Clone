06b4bb8300ffcf261d4c55a316d488bd68070581
nimrahSohail <nimrah.sohail20@gmail.com>
2023-10-15 14:59:40 +0500

    Version 1

---
+++ src/components/Feed.jsx
@@ -0,0 +1,55 @@
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import { SideBar, Videos } from "./index";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
+function Feed() {
+
+  const [selectedCategory, setselectedCategory] = useState('New');
+  const [videos, setvideos] = useState([])
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+    .then((data) => setvideos(data.items))
+  }, [selectedCategory]);
+
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: " auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <SideBar 
+        selectedCategory = {selectedCategory}
+        setselectedCategory={setselectedCategory}
+        />
+
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright 2023 nimrahSohail
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: "auto", height: "92vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          {selectedCategory} <span style={{ color: "#f31503" }}>videos</span>
+        </Typography>
+
+        <Videos videos={videos} />
+      </Box>
+    </Stack>
+  );
+}
+
+export default Feed;
